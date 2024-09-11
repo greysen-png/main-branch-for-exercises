9b631b88d92bda40d78e28c5795ebd90c7c75f7a
Greysen <gr10689@email.vccs.edu>
2024-09-11 23:32:50 +0000

    did the assignment

--- .ccls-cache/@home@runner@main-branch-for-exercises/main.cpp
+++ .ccls-cache/@home@runner@main-branch-for-exercises/main.cpp
@@ -1,3 +1,16 @@
+// Greysen Roy-Pascoe
+// 9/11/2024
+// Checkpoints assignment thing
+// Branch Checkpoint 2.20
 #include <iostream>
 
-int main() { std::cout << "Hello World!\n"; }
+using namespace std;
+
+int main()
+{
+  int age = 19;
+  float weight = 110;
+
+  cout << "What's up my brewskis? It's ya boi Greysen. I'm " << age << " years old and I weigh " << weight << " pounds or something like that." << endl;
+  return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
-//The main branch for some exercises
+// Greysen Roy-Pascoe
+// 9/11/2024
+// Checkpoints assignment thing
+// Branch Checkpoint 2.20
 #include <iostream>
 
 using namespace std;
 
 int main()
+{
+  int age = 19;
+  float weight = 110;
+
+  cout << "What's up my brewskis? It's ya boi Greysen. I'm " << age << " years old and I weigh " << weight << " pounds or something like that." << endl;
+  return 0;
+}
