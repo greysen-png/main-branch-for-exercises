92f094432302185386bf2ed2b98e6246cbfb8893
Greysen <gr10689@email.vccs.edu>
2024-09-11 22:58:50 +0000

    wrote what the assignment wanted

--- .ccls-cache/@home@runner@main-branch-for-exercises/main.cpp
+++ .ccls-cache/@home@runner@main-branch-for-exercises/main.cpp
@@ -1,3 +1,15 @@
+// Greysen Roy-Pascoe
+// 9/11/2024
+// Project with all the checkpoints (like this one)
+// Branch Checkpoint 2.17
 #include <iostream>
 
-int main() { std::cout << "Hello World!\n"; }
+using namespace std;
+
+int main()
+{
+  string name = "Greysen Roy-Pascoe", address = "123 Main Street", phone = "501-123-4567";
+    // Should I have put my real information here?
+  
+  cout << name << endl << address << endl << phone << endl;
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
-//The main branch for some exercises
+// Greysen Roy-Pascoe
+// 9/11/2024
+// Project with all the checkpoints (like this one)
+// Branch Checkpoint 2.17
 #include <iostream>
 
 using namespace std;
 
 int main()
+{
+  string name = "Greysen Roy-Pascoe", address = "123 Main Street", phone = "501-123-4567";
+    // Should I have put my real information here?
+  
+  cout << name << endl << address << endl << phone << endl;
+}
